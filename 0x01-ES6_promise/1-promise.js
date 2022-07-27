952f5e6e4b86d6dedeb50d36d5dd163f400294fd
juan-gimenez <juancitogimenez0@gmail.com>
2022-07-27 18:17:22 +0000

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,13 @@
 export default function getgetFullResponseFromApi(success)  {
-  return new Promise((resolve, reject) => {
-    /* Return a Promise  */
-      if (success == NULL) {
-        reject(new Error('The fake API is not working currently'));
-      } else {
-        resolve({
-	    status: 200,
-	    body: 'Success',
-	});
-      }
-  });
+    return new Promise((resolve, reject) => {
+	/* Return a Promise  */
+	if (success == NULL) {
+            reject(new Error('The fake API is not working currently'));
+	} else {
+            resolve({
+		status: 200,
+		body: 'Success',
+	    });
+	}
+    });
 }
