52db9c96252e6ba77f77fd86d36342c8ef48c711
juan-gimenez <juancitogimenez0@gmail.com>
2022-07-27 18:12:39 +0000

    task 0 and 1 promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI() {
+    /* Return a Promise  */
+    return new Promise((resolve, reject) => {
+        if (true) {
+	    resolve();
+	} else {
+	    reject();
+	}
+  });
+}
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getgetFullResponseFromApi(success)  {
+  return new Promise((resolve, reject) => {
+    /* Return a Promise  */
+      if (success == NULL) {
+        reject(new Error('The fake API is not working currently'));
+      } else {
+        resolve({
+	    status: 200,
+	    body: 'Success',
+	});
+      }
+  });
+}
